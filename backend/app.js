cadc029efd6ec034fc1dcb29a76e19857b61f1df
Rodagoal <48735840+Rodagoal@users.noreply.github.com>
2022-05-02 14:46:37 -0500

    Nuevo

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const cors = require("cors"); 
+const express = require("express");
+const app = express();
+const port = 5000;
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: "tedi"
+});
+connection.connect();
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(cors());
+
+
+//post mandar
+//res peticion original
+//response para mandar datos
+app.post('/login', (
+    req, res) => {
+        connection.query('SELECT * from Admin', (err, response, fields) => {
+            if(err) console.log(err)
+            else {
+      res.send(response)
+      }
+    })
+})
+
+app.listen(port, function(){
+    console.log("express server running on port ${port}!");
+});
---
+++ frontend/src/Pages/Login/Login.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import axios from 'axios';
+import './login.css';
+import  Logo from '../../assets/Logo.jpg';
+
+
+
+class Login extends React.Component{
+   
+  constructor(props){
+    super(props);
+    this.state={
+      user:'',
+      pwd:''
+
+    }
+
+  }
+  
+
+  componentDidMount(){
+    this.caliz()
+  }
+
+
+    handleChange=(e)=>{
+        console.log(e.target.value)
+     
+        this.setState({[e.target.name]:e.target.value})
+        
+
+    }
+    caliz (){
+
+      axios.post("http://localhost:5000/login")
+      .then(res =>{
+        if(res.data){
+          console.log(res.data)
+        }
+        else{
+        }
+        return;
+      })
+    }
+
+  
+
+    loginUser () {
+
+      let usuario = this.state.user;
+      let pass = this.state.password;
+
+
+      axios.post("http://localhost:5000/login", {usuario,  pass})
+      .then(res => {
+
+        if(res.data.result){
+          this.props.setToken(res.data.token);
+          this.props.history.push('/')
+        }
+        else{
+          this.setState({
+            error: true,
+          })
+        }
+        return;
+      })
+  }
+
+
+    handleSubmit=(e)=>{
+            e.preventDefault()
+            this.loginUser();
+    }
+
+
+    render(){
+        return(
+            
+                
+                <div className='div-login'>
+
+                    <div >
+                       <img
+                       src={Logo}
+                       width="95%"
+                       height="95%"
+                       className="align-center"
+                       alt="React Bootstrap logo"
+                       
+                       />
+                    </div>
+
+                    <form onSubmit ={this.handleSubmit}>
+                        <input className='input' type='user' name='user' placeholder='Usuario...' required onChange={this.handleChange}></input>
+                        <input className='input' type='password' name='pwd' placeholder='Contraseña...' required onChange={this.handleChange}></input>
+
+                        <button className='button' onSubmit ={this.handleSubmit}>Ingresar</button>
+
+                    </form>
+
+
+                </div>
+        )
+    }
+}
+
+export default Login;
